be45accf8df5b10a60d476e105c39759b054d280
oscar <oscar.mues@student.kuleuven.be>
2025-05-15 12:27:33 +0200

    edited StavaApiController tests

--- tests/Controller/StravaApi/CallbackWebTest.php
+++ tests/Controller/StravaApi/CallbackWebTest.php
@@ -2,15 +2,67 @@
 
 namespace App\Tests\Controller\StravaApi;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpClient\Exception\TransportException;
+use Symfony\Component\HttpFoundation\Exception\RequestExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class CallbackWebTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testCallbackSuccessWithExistingUser()
+    {
+        $client = static::createClient();
+
+        $mockTokenResponse = $this->createMock(ResponseInterface::class);
+        $mockTokenResponse->method('toArray')->willReturn([
+            'access_token' => 'dummy_token',
+        ]);
+
+        $mockAthleteResponse = $this->createMock(ResponseInterface::class);
+        $mockAthleteResponse->method('toArray')->willReturn([
+            'id' => 1234,
+            'username' => 'stravatestuser',
+            'firstname' => 'Test',
+            'lastname' => 'User',
+        ]);
+
+        $mockHttpClient = $this->createMock(HttpClientInterface::class);
+        $mockHttpClient->method('request')->willReturnCallback(function ($method, $url) use ($mockTokenResponse, $mockAthleteResponse) {
+            if (str_contains($url, 'oauth/token')) {
+                return $mockTokenResponse;
+            }
+
+            if (str_contains($url, 'api/v3/athlete')) {
+                return $mockAthleteResponse;
+            }
+
+            throw new \Exception("Unexpected URL in test: " . $url);
+        });
+
+        $user = new User();
+        $user->setUsername('testuser');
+
+        $mockUserRepository = $this->createMock(UserRepository::class);
+        $mockUserRepository->method('findOneBy')->willReturn($user);
+
+        // Mock EntityManager to prevent actual DB operations
+        $mockEntityManager = $this->createMock(EntityManagerInterface::class);
+
+        self::getContainer()->set(UserRepository::class, $mockUserRepository);
+        self::getContainer()->set(EntityManagerInterface::class, $mockEntityManager);
+        self::getContainer()->set(HttpClientInterface::class, $mockHttpClient);
+
+        $client->request('GET', '/strava/callback?code=xyz');
+
+        $this->assertResponseRedirects('/');
+    }
+
+    public function testCallbackSuccesWithoutExistingUser()
     {
         $client = static::createClient();
 
@@ -43,7 +95,6 @@ class CallbackWebTest extends WebTestCase
             throw new \Exception("Unexpected URL in test: " . $url);
         });
 
-        // Mock UserRepository to simulate "user not found"
         $mockUserRepository = $this->createMock(UserRepository::class);
         $mockUserRepository->method('findOneBy')->willReturn(null);
 
@@ -66,4 +117,18 @@ class CallbackWebTest extends WebTestCase
         // Check that it redirected due to `if ($code == 'xyz')` branch
         $this->assertResponseRedirects('/');
     }
+
+    public function testCallbackUserHasNotGivenPermissionToStravaApi()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/strava/callback?error=access_denied');
+        $this->assertResponseRedirects('/connect_strava');
+    }
+
+    public function testCallbackWithNoCode(){
+        $client = static::createClient();
+        $client->request('GET', '/strava/callback');
+        $this->assertResponseRedirects('/connect_strava');
+    }
+
 }
